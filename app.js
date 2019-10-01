8538db7ba940d14da120390c698a10c76946bb70
Leandro Rodriguez <lrodriguezjb@gmail.com>
2019-10-01 13:47:20 -0700

    Added Javascript file and Stylesheet along with a modification to my index

---
+++ app.js
@@ -0,0 +1,54 @@
+'use strict';
+var onlineUser = prompt('What is your name?');
+alert('Hello '+ onlineUser + ' welcome to the guessing game!');
+
+var whatIsHobbie = prompt('Do you know if I like to make Music');
+whatIsHobbie = whatIsHobbie.toLowerCase();
+if(whatIsHobbie === 'yes') {
+  // console.log('Awesome you guessed right!'); //
+  alert('Awesome you guessed right!');
+} else {
+  // console.log('You Guessed Wrong my hobbie is MUSIC!'); //
+  alert('You Guessed Wrong my hobbie is MUSIC!');
+}
+
+var otherLanguage = prompt('Other than English do I speak Spanish?');
+otherLanguage = otherLanguage.toLowerCase();
+if(otherLanguage === 'yes') {
+  // console.log('Gracias! You are correcto!'); //
+  alert('Gracias! You are Correcto!');
+} else {
+  // console.log('You Guessed Wrong I speak Spanish'); //
+  alert('You Guessed Wrong I Speak Spanish');
+}
+
+var militaryBranch = prompt('Was I in the Army?');
+militaryBranch = militaryBranch.toLowerCase();
+if(militaryBranch === 'no') {
+  // console.log('Hooyah! You are Correct'); //
+  alert('Hooyah! You are Correct');
+} else {
+  // console.log('You Guessed Wrong I Served in the Navy'); //
+  alert('You Guessed Wrong I Served In the Navy');
+}
+
+var favoriteFood = prompt('Is Pizza my Favorite?');
+favoriteFood = favoriteFood.toLowerCase();
+if(favoriteFood === 'yes') {
+  // console.log('Correct!! Pizza is one of the best things in the world! and my Favorite!'); //
+  alert('Correct!! Pizza is one of the best things in the world! and my Favorite!');
+} else {
+  // console.log('You Guessed Wrong I actually love Pizza'); //
+  alert('You Guessed Wrong I actually love Pizza');
+}
+
+var favoriteColor = prompt('Is my Favorite Color Purple?'); 
+favoriteColor = favoriteColor.toLowerCase();
+if(favoriteColor === 'no') {
+  // console.log('Correct! My Favorite Color is Green!'); //
+  alert('Correct! My Favorite Color is Green!');
+} else {
+  // console.log('You Guessed Wrong! My favorite Color is Green'); //
+  alert('You Guessed Wrong! My Favorite Color is Green');
+}
+
